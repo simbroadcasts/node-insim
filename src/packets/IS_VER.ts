a6fc75765fcee9f650342c47d7ff81c75b766003
Martin Kapal <flamecze@gmail.com>
2021-12-26 23:41:27 +0100

    Fix IS_VER packet type

--- src/packets/IS_VER.ts
+++ src/packets/IS_VER.ts
@@ -13,7 +13,7 @@ export type IS_VER_Data = {
 export class IS_VER extends BasePacket implements IS_VER_Data {
   readonly _format = '<BBBB8s6sH';
   readonly Size = 20;
-  readonly Type = PacketType.ISP_ISI;
+  readonly Type = PacketType.ISP_VER;
   ReqI = 0;
   Zero = 0;
   Version = '0';
