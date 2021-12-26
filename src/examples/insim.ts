e1a6ab3874b346f9a9f9e4346dd78d10d8a25748
Martin Kapal <flamecze@gmail.com>
2021-12-26 23:32:47 +0100

    Move example usage to a separate folder

---
+++ src/examples/insim.ts
@@ -0,0 +1,22 @@
+import NodeInSim, { PacketType } from '../index';
+import { InSim } from '../InSim';
+import { IS_ISI_ReqI } from '../packets/IS_ISI';
+import { IS_VER } from '../packets/IS_VER';
+
+const inSim = new NodeInSim.InSim();
+
+inSim.connect({
+  Host: '10.0.0.100',
+  Port: 29998,
+  ReqI: IS_ISI_ReqI.REQI_SEND_VERSION,
+  IName: 'Test Node App',
+});
+
+function onVersion(packet: IS_VER, insim: InSim) {
+  console.log('LFS version', packet.Version);
+}
+
+inSim.on(PacketType[PacketType.ISP_VER], onVersion);
+inSim.on('error', (error) => {
+  console.error('Error:', error);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import { InSim } from './InSim';
-import { IS_ISI_ReqI } from './packets/IS_ISI';
-import { IS_VER } from './packets/IS_VER';
 import {
   PacketType,
   SmallType,
@@ -11,25 +9,6 @@ import {
 const NodeInSim = {
   InSim,
 };
-
-// Test usage
-const inSim = new NodeInSim.InSim();
-
-inSim.connect({
-  Host: '10.0.0.100',
-  Port: 29998,
-  ReqI: IS_ISI_ReqI.REQI_SEND_VERSION,
-  IName: 'Test Node App',
-});
-
-function onVersion(packet: IS_VER, insim: InSim) {
-  console.log('LFS version', packet.Version);
-}
-
-inSim.on(PacketType[PacketType.ISP_VER], onVersion);
-inSim.on('error', (error) => {
-  console.error('Error:', error);
-});
 
 export { PacketType, SmallType, TinyType, TtcType };
 
