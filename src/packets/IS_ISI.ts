5624476d7ecf0cf7bced8cad487045caf1207180
Martin Kapal <flamecze@gmail.com>
2021-12-31 15:27:23 +0100

    Add JSDoc comments to packets

--- src/packets/IS_ISI.ts
+++ src/packets/IS_ISI.ts
@@ -1,18 +1,39 @@
 import { BaseSendablePacket } from './BaseSendablePacket';
 import { PacketType } from './packetTypes';
 
+/**
+ * InSim Init - packet to initialise the InSim system
+ */
 export class IS_ISI extends BaseSendablePacket implements IS_ISI_Data {
   readonly _format = '<BBBxHHBcH16s16s';
+
   Size = 44;
   Type = PacketType.ISP_ISI;
+
+  /** If non-zero LFS will send an IS_VER packet */
   ReqI = 0;
+
   readonly Zero = 0;
+
+  /** Port for UDP replies from LFS (0 to 65535) */
   UDPPort = 0;
+
+  /** Bit flags for options */
   Flags: InSimFlags = 0;
+
+  /** The INSIM_VERSION used by your program */
   InSimVer = 0;
+
+  /** Special host message prefix character */
   Prefix = '';
+
+  /** Time in ms between {@link IS_NLP} or {@link IS_MCI} (0 = none) */
   Interval = 0;
+
+  /** Admin password (if set in LFS) */
   Admin = '';
+
+  /** A short name for your program */
   IName = '';
 
   constructor(data?: Partial<IS_ISI_Data>) {
@@ -22,32 +43,72 @@ export class IS_ISI extends BaseSendablePacket implements IS_ISI_Data {
 }
 
 export type IS_ISI_Data = {
+  /** If non-zero LFS will send an {@link IS_VER} packet */
   ReqI: IS_ISI_ReqI;
+
+  /** Port for UDP replies from LFS (0 to 65535) */
   UDPPort: number;
+
+  /** Bit flags for options */
   Flags: InSimFlags;
+
+  /** The InSim version used by your program */
   InSimVer: number;
+
+  /** Special host message prefix character */
   Prefix: string;
+
+  /** Time in ms between {@link IS_NLP} or {@link IS_MCI} (0 = none) */
   Interval: number;
+
+  /** Admin password (if set in LFS) */
   Admin: string;
+
+  /** A short name for your program */
   IName: string;
 };
 
 export enum IS_ISI_ReqI {
   ZERO,
+
+  /** Send back an {@link IS_VER} packet */
   SEND_VERSION,
 }
 
 export enum InSimFlags {
-  ISF_RES_0 = 1, // bit  0: spare
-  ISF_RES_1 = 2, // bit  1: spare
-  ISF_LOCAL = 4, // bit  2: guest or single player
-  ISF_MSO_COLS = 8, // bit  3: keep colours in MSO text
-  ISF_NLP = 16, // bit  4: receive NLP packets
-  ISF_MCI = 32, // bit  5: receive MCI packets
-  ISF_CON = 64, // bit  6: receive CON packets
-  ISF_OBH = 128, // bit  7: receive OBH packets
-  ISF_HLV = 256, // bit  8: receive HLV packets
-  ISF_AXM_LOAD = 512, // bit  9: receive AXM when loading a layout
-  ISF_AXM_EDIT = 1024, // bit 10: receive AXM when changing objects
-  ISF_REQ_JOIN = 2048, // bit 11: process join requests
+  /** Spare */
+  ISF_RES_0 = 1,
+
+  /** Spare */
+  ISF_RES_1 = 2,
+
+  /** Guest or single player */
+  ISF_LOCAL = 4,
+
+  /** Keep colours in {@link IS_MSO} text */
+  ISF_MSO_COLS = 8,
+
+  /** Receive {@link IS_NLP} packets */
+  ISF_NLP = 16,
+
+  /** Receive {@link IS_MCI} packets */
+  ISF_MCI = 32,
+
+  /** Receive {@link IS_CON} packets */
+  ISF_CON = 64,
+
+  /** Receive {@link IS_OBH} packets */
+  ISF_OBH = 128,
+
+  /** Receive {@link IS_HLV} packets */
+  ISF_HLV = 256,
+
+  /** Receive {@link IS_AXM} when loading a layout */
+  ISF_AXM_LOAD = 512,
+
+  /** Receive {@link IS_AXM} when changing objects */
+  ISF_AXM_EDIT = 1024,
+
+  /** Process join requests */
+  ISF_REQ_JOIN = 2048,
 }
--- src/packets/IS_VER.ts
+++ src/packets/IS_VER.ts
@@ -1,23 +1,33 @@
 import { BasePacket } from './BasePacket';
 import { PacketType } from './packetTypes';
 
-export type IS_VER_Data = {
-  ReqI: number;
-  Version: string;
-  Product: string;
-  InSimVer: number;
-  Spare: number;
-};
-
+/**
+ * Version request
+ *
+ * It is advisable to request version information as soon as you have connected, to
+ * avoid problems when connecting to a host with a later or earlier version. You will
+ * be sent a version packet on connection if you set ReqI in the {@link IS_ISI} packet.
+ */
 export class IS_VER extends BasePacket implements IS_VER_Data {
   readonly _format = '<BBBB8s6sBB';
+
   readonly Size = 20;
   readonly Type = PacketType.ISP_VER;
+
+  /** ReqI as received in the request packet */
   ReqI = 0;
+
   readonly Zero = 0;
+
+  /** LFS version, e.g. 0.3G */
   Version = '';
+
+  /** Product: DEMO / S1 / S2 / S3 */
   Product = '';
+
+  /** InSim version */
   InSimVer = 0;
+
   readonly Spare = 0;
 
   constructor(data?: Partial<IS_VER_Data>) {
@@ -25,3 +35,19 @@ export class IS_VER extends BasePacket implements IS_VER_Data {
     this.populateData(data);
   }
 }
+
+export type IS_VER_Data = {
+  /** ReqI as received in the request packet */
+  ReqI: number;
+
+  /** LFS version, e.g. 0.3G */
+  Version: string;
+
+  /** Product: DEMO / S1 / S2 / S3 */
+  Product: string;
+
+  /** InSim version */
+  InSimVer: number;
+
+  Spare: number;
+};
