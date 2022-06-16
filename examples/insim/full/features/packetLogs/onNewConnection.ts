c58738f66aecf6f54113f0d2fe45a69376e7eb99
Martin Kapal <flamecze@gmail.com>
2022-06-16 22:25:25 +0200

    Fix onNewConnection log

--- examples/insim/full/features/packetLogs/onNewConnection.ts
+++ examples/insim/full/features/packetLogs/onNewConnection.ts
@@ -2,5 +2,5 @@ import type { IS_NCN } from '../../../../../src/packets';
 import { log } from '../../log';
 
 export function onNewConnection(packet: IS_NCN) {
-  log.info('Received IS_NCN packet', packet.UName);
+  log.info(`Received IS_NCN packet - ${packet.UName}`);
 }
