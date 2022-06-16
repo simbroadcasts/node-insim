7d66c55b9d8b59a8e543e356058137350e4ca038
Martin Kapal <flamecze@gmail.com>
2022-06-16 22:19:47 +0200

    Only print error name and message in full example

--- examples/insim/full/index.ts
+++ examples/insim/full/index.ts
@@ -28,7 +28,7 @@ function startInSim(host = '127.0.0.1', port = 29999) {
 
   inSim.on('connect', () => log.info('Connected'));
   inSim.on('disconnect', () => log.info('Disconnected'));
-  inSim.on('error', (error) => log.error('Error:', error));
+  inSim.on('error', (error) => log.error('Error:', error.name, error.message));
 
   logPackets(inSim);
   drawTestButtons(inSim);
