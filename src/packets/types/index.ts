b258e84156bcadc8d5ac48af86cb151922c11a0f
Martin Kapal <flamecze@gmail.com>
2022-08-07 11:58:23 +0200

    Refactor packet type utils

--- src/packets/types/index.ts
+++ src/packets/types/index.ts
@@ -1,19 +1,29 @@
 import type { BaseSendablePacket } from '../BaseSendablePacket';
 import type { IPacket } from '../IPacket';
 
-type ReadonlySendableProps = keyof BaseSendablePacket | 'Zero';
+type ReadonlyPropNames =
+  | 'Zero'
+  | 'Spare'
+  | 'Spare1'
+  | 'Spare2'
+  | 'Spare3'
+  | 'Sp1'
+  | 'Sp2'
+  | 'Sp3';
+
+type ReadonlySendableProps = keyof BaseSendablePacket | ReadonlyPropNames;
 
 type ReadonlySendablePropsExceptReqI =
   | keyof Omit<BaseSendablePacket, 'ReqI'>
-  | 'Zero';
+  | ReadonlyPropNames;
 
 type OmitReadonlyProps<T> = Omit<T, ReadonlySendableProps>;
 
 type OmitReadonlyPropsExceptReqI<T> = Omit<T, ReadonlySendablePropsExceptReqI>;
 
-export type PacketData<T extends IPacket> = Partial<OmitReadonlyProps<T>>;
-
 type PacketDataWithReqI<T extends IPacket> = OmitReadonlyPropsExceptReqI<T>;
+
+export type PacketData<T extends IPacket> = Partial<OmitReadonlyProps<T>>;
 
 export type PacketDataWithRequiredReqI<T extends IPacket> = Partial<
   Omit<PacketDataWithReqI<T>, 'ReqI'>
