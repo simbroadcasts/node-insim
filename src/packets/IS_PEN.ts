d1272b84683612e4eff7c551bb16ecdc223fcde2
Martin Kapal <flamecze@gmail.com>
2022-12-27 08:56:35 +0100

    IS_PEN - add comment about false start / wrong route

--- src/packets/IS_PEN.ts
+++ src/packets/IS_PEN.ts
@@ -5,6 +5,11 @@ import { PacketType } from './enums';
 
 /**
  * PENalty (given or cleared)
+ *
+ * On false start or wrong route / restricted area, an IS_PEN packet is sent:
+ *
+ * - False start: OldPen = 0 / NewPen = {@link PENALTY_30} / Reason = {@link PENR_FALSE_START}
+ * - Wrong route: OldPen = 0 / NewPen = {@link PENALTY_45} / Reason = {@link PENR_WRONG_WAY}
  */
 export class IS_PEN extends AbstractPacket {
   @byte() readonly Size = 8;
