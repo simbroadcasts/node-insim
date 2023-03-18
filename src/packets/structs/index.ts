a06b7655d231afe8d94e9f7e5c5285d788b272f3
Martin Kapal <flamecze@gmail.com>
2023-03-18 23:25:31 +0100

    Add HInfo to index

--- src/packets/structs/index.ts
+++ src/packets/structs/index.ts
@@ -2,5 +2,6 @@ export { CarContact } from './CarContact';
 export { CarContOBJ } from './CarContOBJ';
 export { CarHCP } from './CarHCP';
 export { CompCar } from './CompCar';
+export { HInfo } from './HInfo';
 export { NodeLap } from './NodeLap';
 export { ObjectInfo } from './ObjectInfo';
