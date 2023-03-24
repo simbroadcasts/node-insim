657be76c0371c170066a6ce9ff8eac3d55b6ebfe
Martin Kapal <flamecze@gmail.com>
2023-03-24 14:32:38 +0100

    Extract build script into scripts/build.js

---
+++ scripts/build.js
@@ -0,0 +1,31 @@
+import * as esbuild from 'esbuild';
+
+const commonConfig = {
+  entryPoints: [
+    'src/index.ts',
+    'src/packets/index.ts',
+    'src/protocols/index.ts',
+  ],
+  bundle: true,
+  platform: 'node',
+  target: 'node18',
+  packages: 'external',
+  outdir: 'dist',
+  tsconfig: 'tsconfig.app.json',
+};
+
+await esbuild.build({
+  ...commonConfig,
+  format: 'cjs',
+  outExtension: {
+    '.js': '.cjs',
+  },
+});
+
+await esbuild.build({
+  ...commonConfig,
+  format: 'esm',
+  outExtension: {
+    '.js': '.esm.js',
+  },
+});
