d390cbefec600f5c3abff83c3b3ec88b18d306f5
Martin Kapal <flamecze@gmail.com>
2023-03-24 12:11:03 +0100

    Remove unused `KeyOfType`

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,3 @@
 export * from './decorators';
 export * from './lfspack';
 export * from './log';
-
-export type KeyOfType<T, V> = keyof {
-  [P in keyof T as T[P] extends V ? P : never]: unknown;
-};
