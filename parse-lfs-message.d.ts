050be7610652ab0166dfc26992d129c398468b4d
Martin Kapal <flamecze@gmail.com>
2023-03-29 14:51:11 +0200

    Upgrade `parse-lfs-message` to v1.2.0

--- parse-lfs-message.d.ts
+++
@@ -1,5 +0,0 @@
-// TODO: Remove when 'parse-lfs-message' is converted to TypeScript
-declare module 'parse-lfs-message' {
-  function parseLFSMessage(msg: string | Uint8Array);
-  export = parseLFSMessage;
-}
