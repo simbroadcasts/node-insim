3f2f7aa09631ce530e9ac0007a4e4d1c1de41305
Martin Kapal <flamecze@gmail.com>
2023-04-03 01:16:20 +0200

    Fix DashLights enum values

--- src/out/DashLights.ts
+++ src/out/DashLights.ts
@@ -1,38 +1,38 @@
 export enum DashLights {
   /** Shift light */
-  DL_SHIFT = 0,
+  DL_SHIFT = 1,
 
   /** Full beam */
-  DL_FULLBEAM = 1,
+  DL_FULLBEAM = 2,
 
   /** Handbrake */
-  DL_HANDBRAKE = 2,
+  DL_HANDBRAKE = 4,
 
   /** Pit speed limiter */
-  DL_PITSPEED = 4,
+  DL_PITSPEED = 8,
 
   /** TC active or switched off */
-  DL_TC = 8,
+  DL_TC = 16,
 
   /** Left turn signal */
-  DL_SIGNAL_L = 16,
+  DL_SIGNAL_L = 32,
 
   /** Right turn signal */
-  DL_SIGNAL_R = 32,
+  DL_SIGNAL_R = 64,
 
   /** Shared turn signal */
-  DL_SIGNAL_ANY = 64,
+  DL_SIGNAL_ANY = 128,
 
   /** Oil pressure warning */
-  DL_OILWARN = 128,
+  DL_OILWARN = 256,
 
   /** Battery warning */
-  DL_BATTERY = 256,
+  DL_BATTERY = 512,
 
   /** ABS active or switched off */
-  DL_ABS = 512,
+  DL_ABS = 1024,
 
-  DL_SPARE = 1024,
+  DL_SPARE = 2048,
 
-  DL_NUM = 2048,
+  DL_NUM = 4096,
 }
