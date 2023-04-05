ee908a8065aff44271228aa379ffb33b5d76ac1b
Martin Kapal <flamecze@gmail.com>
2023-04-05 18:51:02 +0200

    Remove test button packet from InSim example

--- examples/insim-connection-ts/src/index.ts
+++ examples/insim-connection-ts/src/index.ts
@@ -3,7 +3,7 @@ import './env.ts';
 import debug from 'debug';
 import { InSim } from 'node-insim';
 import type { IS_VER } from 'node-insim/packets';
-import { IS_ISI_ReqI, PacketType, IS_BTN } from 'node-insim/packets';
+import { IS_ISI_ReqI, PacketType } from 'node-insim/packets';
 
 const log = debug('node-insim-ts');
 
@@ -25,30 +25,6 @@ inSim.on(PacketType.ISP_VER, onVersion);
 
 function onVersion(packet: IS_VER) {
   log(`Connected to LFS ${packet.Product} ${packet.Version}`);
-
-  inSim.send(
-    new IS_BTN({
-      ReqI: 1,
-      ClickID: 1,
-      T: 2,
-      L: 30,
-      W: 40,
-      H: 10,
-      UCID: 255,
-      Text: convertUtf8ToLFSMessage('tščř'),
-    }),
-  );
-}
-
-function convertUtf8ToLFSMessage(msg: string) {
-  let lfsMsg = 'e';
-
-  msg.split('').forEach((char) => {
-    const a = new TextEncoder();
-    a.encode(char);
-  });
-
-  return lfsMsg;
 }
 
 process.on('uncaughtException', (error) => {
