5f7098d19cf89e06324c687ddf35c680d7e4c807
Martin Kapal <flamecze@gmail.com>
2023-04-08 12:33:07 +0200

    Remove default export of `NodeInSim`

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-import { InSim } from './InSim';
-
-const NodeInSim = {
-  InSim,
-};
-
-export default NodeInSim;
-
 export type { InSimError } from './errors';
 export type { InSimOptions } from './InSim';
 export { InSim } from './InSim';
