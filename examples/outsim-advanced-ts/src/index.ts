d6f167dabfc09d299ce84ef57ac7fb4312d997f0
Martin Kapal <flamecze@gmail.com>
2023-04-08 10:10:36 +0200

    Log all available data in OutSim advanced example

--- examples/outsim-advanced-ts/src/index.ts
+++ examples/outsim-advanced-ts/src/index.ts
@@ -1,8 +1,7 @@
 import './env.ts';
 
 import debug from 'debug';
-import { OutSim } from 'node-insim';
-import { OutSimPack2 } from 'node-insim';
+import { OutSim, OutSimPack2, OutSimWheel } from 'node-insim';
 
 const log = debug('node-insim-outsim-advanced-ts');
 
@@ -28,22 +27,47 @@ outSim.on('timeout', () => {
 
 outSim.on('packet', (data: OutSimPack2) => {
   console.clear();
-  console.log(
-    `Velocity: ${data.OSMain.Vel[0]} ${data.OSMain.Vel[1]} ${data.OSMain.Vel[2]}`,
-  );
-  console.log(
-    `Airspeed: ${Math.sqrt(
-      Math.pow(data.OSMain.Vel[0], 2) +
-        Math.pow(data.OSMain.Vel[1], 2) +
-        Math.pow(data.OSMain.Vel[2], 2),
-    )} m/s`,
-  );
-  console.log(
-    `Position: ${data.OSMain.Pos[0]} ${data.OSMain.Pos[1]} ${data.OSMain.Pos[2]}`,
-  );
-  console.log(`Gear: ${data.Gear}`);
-  console.log(`Rpm: ${(30 * data.EngineAngVel) / Math.PI}`);
+  row('AngVel', formatVector(data.OSMain.AngVel));
+  row('Heading', formatNumber(data.OSMain.Heading));
+  row('Pitch', formatNumber(data.OSMain.Pitch));
+  row('Roll', formatNumber(data.OSMain.Roll));
+  row('Accel', formatVector(data.OSMain.Accel));
+  row('Vel', formatVector(data.OSMain.Vel));
+  row('Pos', formatVector(data.OSMain.Pos));
+  row('Throttle', formatNumber(data.OSInputs.Throttle));
+  row('Brake', formatNumber(data.OSInputs.Brake));
+  row('InputSteer', formatNumber(data.OSInputs.InputSteer));
+  row('Clutch', formatNumber(data.OSInputs.Clutch));
+  row('Handbrake', formatNumber(data.OSInputs.Handbrake));
+  row('Gear', formatNumber(data.Gear));
+  row('EngineAngVel', formatNumber(data.EngineAngVel));
+  row('MaxTorqueAtVel', formatNumber(data.MaxTorqueAtVel));
+  row('CurrentLapDist', formatNumber(data.CurrentLapDist));
+  row('IndexedDistance', formatNumber(data.IndexedDistance));
+  data.OSWheels.forEach((wheel: OutSimWheel, index: number) => {
+    row(`OSWheels[${index}] - SuspDeflect`, formatNumber(wheel.SuspDeflect));
+    row(`OSWheels[${index}] - Steer`, formatNumber(wheel.Steer));
+    row(`OSWheels[${index}] - XForce`, formatNumber(wheel.XForce));
+    row(`OSWheels[${index}] - YForce`, formatNumber(wheel.YForce));
+    row(`OSWheels[${index}] - VerticalLoad`, formatNumber(wheel.VerticalLoad));
+    row(`OSWheels[${index}] - AngVel`, formatNumber(wheel.AngVel));
+    row(
+      `OSWheels[${index}] - LeanRelToRoad`,
+      formatNumber(wheel.LeanRelToRoad),
+    );
+    row(`OSWheels[${index}] - SlipFraction`, formatNumber(wheel.SlipFraction));
+    row(`OSWheels[${index}] - SlipRatio`, formatNumber(wheel.SlipRatio));
+    row(`OSWheels[${index}] - TanSlipAngle`, formatNumber(wheel.TanSlipAngle));
+  });
+  row('SteerTorque', formatNumber(data.SteerTorque));
 });
+
+const formatVector = (value: number[]) =>
+  value.map((v) => formatNumber(v)).join('');
+const formatNumber = (value: number) => value.toString().padStart(25);
+const formatLabel = (value: string) => `${value}:`.toString().padEnd(28);
+const row = (label: string, data: string) =>
+  console.log(`${formatLabel(label)} ${data}`);
 
 process.on('uncaughtException', (error) => {
   log(error);
