e3a2d4891c6546bfc7e2422980fdb0b79202ddb5
Martin Kapal <makap@ciklum.com>
2023-10-16 22:40:04 +0200

    feat(is_small): improve type safety of IS_SMALL packet data types
    Allow only sendable subtypes of IS_SMALL to be passed in `SubT`. Added static constants for special
    cases of `UVal`. Added JSDoc comments for data of each subtype.

--- src/packets/IS_SMALL.ts
+++ src/packets/IS_SMALL.ts
@@ -1,12 +1,27 @@
 import { byte, unsigned } from '../decorators';
 import { SendablePacket } from './base';
+import type { CarFlags, LocalCarSwitches } from './enums';
 import { PacketType, SmallType } from './enums';
-import type { PacketDataWithOptionalReqI } from './types';
 
 /**
  * General purpose 8 byte packet
  */
 export class IS_SMALL extends SendablePacket {
+  /** Stop sending positions */
+  public static readonly SSP_STOP_SENDING = 0;
+
+  /** Stop sending gauges */
+  public static readonly SSG_STOP_SENDING = 0;
+
+  /** Stop time */
+  public static readonly TMS_STOP = 1;
+
+  /** Start time */
+  public static readonly TMS_CONTINUE = 0;
+
+  /** Stop sending {@link IS_NLP} or {@link IS_MCI} packets */
+  public static readonly NLI_STOP = 0;
+
   @byte() readonly Size = 8;
   @byte() readonly Type = PacketType.ISP_SMALL;
 
@@ -25,7 +40,92 @@ export class IS_SMALL extends SendablePacket {
   }
 }
 
-export type IS_SMALL_Data = PacketDataWithOptionalReqI<IS_SMALL>;
+export type IS_SMALL_Data =
+  | SMALL_SSP_Data
+  | SMALL_SSG_Data
+  | SMALL_TMS_Data
+  | SMALL_STP_Data
+  | SMALL_NLI_Data
+  | SMALL_ALC_Data
+  | SMALL_LCS_Data;
+
+type SMALL_SSP_Data = Data<{
+  ReqI?: never;
+
+  /** Instruction: start sending car positions via OutSim */
+  SubT: SmallType.SMALL_SSP;
+
+  /** Time between updates in milliseconds - zero means stop sending */
+  UVal: number | typeof IS_SMALL.SSP_STOP_SENDING;
+}>;
+
+type SMALL_SSG_Data = Data<{
+  ReqI?: never;
+
+  /** Instruction: start sending gauges */
+  SubT: SmallType.SMALL_SSG;
+
+  /** Time between updates in milliseconds - zero means stop sending */
+  UVal: number | typeof IS_SMALL.SSG_STOP_SENDING;
+}>;
+
+type SMALL_TMS_Data = Data<{
+  ReqI?: never;
+
+  /** Instruction: time stop */
+  SubT: SmallType.SMALL_TMS;
+
+  /** 1 - stop / 0 - carry on */
+  UVal: typeof IS_SMALL.TMS_STOP | typeof IS_SMALL.TMS_CONTINUE;
+}>;
+
+type SMALL_STP_Data = Data<{
+  ReqI?: never;
+
+  /** Instruction: time step */
+  SubT: SmallType.SMALL_STP;
+
+  /** Number of hundredths of a second to update */
+  UVal: number;
+}>;
+
+type SMALL_NLI_Data = Data<{
+  ReqI?: never;
+
+  /** Instruction: set the rate of {@link IS_NLP} or {@link IS_MCI} after initialization */
+  SubT: SmallType.SMALL_NLI;
+
+  /** 0 means stop, otherwise time interval: 40, 50, 60... 8000 ms */
+  UVal: number | typeof IS_SMALL.NLI_STOP;
+}>;
+
+type SMALL_ALC_Data = Data<{
+  ReqI?: never;
+
+  /** Instruction: set allowed cars on the host (like /cars command) */
+  SubT: SmallType.SMALL_ALC;
+
+  /** Allowed cars */
+  UVal: CarFlags;
+}>;
+
+type SMALL_LCS_Data = Data<{
+  ReqI?: never;
+
+  /** Instruction: set local car switches (lights, horn, siren) */
+  SubT: SmallType.SMALL_LCS;
+
+  /** Switches */
+  UVal: LocalCarSwitches;
+}>;
+
+type Data<
+  TData extends {
+    ReqI?: number;
+    SubT: SendableSmallType;
+    UVal: number;
+  },
+> = TData;
 
 export const SENDABLE_SMALL_TYPES = [
   SmallType.SMALL_SSP,
--- src/packets/__tests__/IS_SMALL.test.ts
+++ src/packets/__tests__/IS_SMALL.test.ts
@@ -1,28 +1,27 @@
-import { testInstructionPacket } from '../../tests';
+import { testBothWaysPacket } from '../../tests';
 import type { IS_SMALL_Data } from '..';
-import { IS_SMALL, PacketType, SmallType } from '..';
+import { CarFlags, IS_SMALL, PacketType, SmallType } from '..';
 
 const size = 8;
 
 const data: IS_SMALL_Data = {
-  ReqI: 1,
-  SubT: SmallType.SMALL_NLI,
-  UVal: 257,
+  SubT: SmallType.SMALL_ALC,
+  UVal: CarFlags.BF1 | CarFlags.RAC,
 };
 
 const buffer = new Uint8Array([
   size / new IS_SMALL().SIZE_MULTIPLIER, // Size
   4, // Type
-  1, // ReqI
-  7, // SubT
-  1, // UVal (1)
-  1, // UVal (2)
-  0, // UVal (3)
+  0, // ReqI
+  8, // SubT
+  0, // UVal (1)
+  2, // UVal (2)
+  4, // UVal (3)
   0, // UVal (4)
 ]);
 
 describe('IS_SMALL', () => {
-  testInstructionPacket({
+  testBothWaysPacket({
     packetClass: IS_SMALL,
     size,
     type: PacketType.ISP_SMALL,
