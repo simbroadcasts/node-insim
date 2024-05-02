9ebc3e09159c0e5ecbb2f2a407207ef4993f764f
Martin Kapal <flamecze@gmail.com>
2024-05-02 09:40:38 +0200

    chore(insim): add unit tests

---
+++ src/InSim.test.ts
@@ -0,0 +1,273 @@
+import Mitm from 'mitm';
+import { InSim } from 'node-insim';
+import { InSimFlags, IS_TINY, PacketType, TinyType } from 'node-insim/packets';
+
+import { stringToBytes } from './tests';
+
+describe('InSim', () => {
+  let mitm: ReturnType<typeof Mitm>;
+
+  beforeEach(() => {
+    mitm = Mitm();
+  });
+
+  it('should be exported', () => {
+    expect(InSim).toBeDefined();
+    expect(typeof InSim).toEqual('function');
+  });
+
+  it('should expose INSIM_VERSION equal to 9', () => {
+    expect(InSim.INSIM_VERSION).toEqual(9);
+  });
+
+  describe('options', () => {
+    it('should have default values', () => {
+      const insim = new InSim();
+      expect(insim.options).toEqual({
+        Host: '127.0.0.1',
+        Port: 29999,
+        Protocol: 'TCP',
+        ReqI: 0,
+        UDPPort: 0,
+        Flags: 0,
+        Prefix: '',
+        Interval: 0,
+        Admin: '',
+        IName: '',
+      });
+    });
+  });
+
+  describe('id', () => {
+    it('should be a random string by default', () => {
+      const inSim = new InSim();
+      expect(typeof inSim.id).toEqual('string');
+    });
+
+    it('should be configurable', () => {
+      const inSim = new InSim('foo');
+      expect(inSim.id).toEqual('foo');
+    });
+  });
+
+  describe('InSim connection', () => {
+    it('should connect using TCP and send an IS_ISI packet', (done) => {
+      const inSim = new InSim();
+
+      mitm.on('connection', (_socket, opts) => {
+        expect(opts.host).toEqual('127.0.0.1');
+        expect(opts.port).toEqual(29999);
+
+        inSim.disconnect();
+        done();
+      });
+
+      inSim.connect({
+        Host: '127.0.0.1',
+        Port: 29999,
+      });
+    });
+
+    it('should connect using custom options', (done) => {
+      const inSim = new InSim();
+
+      mitm.on('connection', (socket, opts) => {
+        expect(opts.host).toEqual('192.161.4.1');
+        expect(opts.port).toEqual(29998);
+
+        socket.on('data', (data) => {
+          expect(data).toEqual(
+            Buffer.from([
+              11, // Size / 4
+              1, // PacketType.ISP_ISI
+              3, // ReqI
+              0, // Zero
+              45, // UDPPort (1)
+              117, // UDPPort (2)
+              64, // InSimFlags.ISF_CON (1)
+              0, // InSimFlags.ISF_CON (2)
+              9, // InSimVer
+              '!'.charCodeAt(0), // Prefix
+              10, // Interval (1)
+              0, // Interval (2)
+              ...stringToBytes('adminPassword\0\0\0'), // Admin[16]
+              ...stringToBytes('Node InSim\0\0\0\0\0\0'), // IName[16]
+            ]),
+          );
+
+          inSim.disconnect();
+          done();
+        });
+      });
+
+      inSim.connect({
+        Host: '192.161.4.1',
+        Port: 29998,
+        ReqI: 3,
+        UDPPort: 29997,
+        Flags: InSimFlags.ISF_CON,
+        Prefix: '!',
+        Interval: 10,
+        Admin: 'adminPassword',
+        IName: 'Node InSim',
+      });
+
+      expect(inSim.options).toEqual({
+        Host: '192.161.4.1',
+        Port: 29998,
+        ReqI: 3,
+        UDPPort: 29997,
+        Flags: InSimFlags.ISF_CON,
+        Prefix: '!',
+        Protocol: 'TCP',
+        Interval: 10,
+        Admin: 'adminPassword',
+        IName: 'Node InSim',
+      });
+    });
+
+    it('should connect only once', (done) => {
+      const inSim = new InSim();
+
+      mitm.on('connection', () => {
+        inSim.disconnect();
+        done();
+      });
+
+      inSim.connect({
+        Host: '127.0.0.1',
+        Port: 29999,
+      });
+      inSim.connect({
+        Host: '127.0.0.1',
+        Port: 29999,
+      });
+    });
+  });
+
+  describe('InSim Relay connection', () => {
+    it('should connect using TCP', (done) => {
+      const inSim = new InSim();
+
+      mitm.on('connection', (_socket, opts) => {
+        expect(opts.host).toEqual('isrelay.lfs.net');
+        expect(opts.port).toEqual(47474);
+
+        inSim.disconnect();
+        done();
+      });
+
+      inSim.connectRelay();
+    });
+  });
+
+  describe('sending packets', () => {
+    let mitm: ReturnType<typeof Mitm>;
+
+    beforeEach(() => {
+      mitm = Mitm();
+    });
+
+    it('should send a packet', (done) => {
+      const inSim = new InSim();
+
+      mitm.on('connection', (socket) => {
+        socket.on('data', (data) => {
+          expect(data).toEqual(
+            Buffer.from([
+              1, // Size / 4
+              3, // PacketType.ISP_TINY
+              25, // ReqI
+              3, // TinyType.TINY_PING
+            ]),
+          );
+          inSim.disconnect();
+          done();
+        });
+      });
+
+      inSim.connect({
+        Host: '127.0.0.1',
+        Port: 29999,
+      });
+
+      inSim.send(
+        new IS_TINY({
+          ReqI: 25,
+          SubT: TinyType.TINY_PING,
+        }),
+      );
+    });
+  });
+
+  describe('receiving packets', () => {
+    let mitm: ReturnType<typeof Mitm>;
+
+    beforeEach(() => {
+      mitm = Mitm();
+    });
+
+    it('should receive a packet', (done) => {
+      const inSim = new InSim();
+
+      inSim.on(PacketType.ISP_TINY, (packet) => {
+        expect(packet.SubT).toEqual(TinyType.TINY_REPLY);
+        done();
+      });
+
+      mitm.on('connection', (socket) => {
+        socket.write(
+          new Uint8Array([
+            1, // Size / 4
+            3, // PacketType.ISP_TINY
+            25, // ReqI
+            4, // TinyType.TINY_PING
+          ]),
+        );
+      });
+
+      inSim.connect({
+        Host: '127.0.0.1',
+        Port: 29999,
+      });
+    });
+  });
+
+  describe('keep-alive packet', () => {
+    it('should respond to a TINY_NONE packet', (done) => {
+      const inSim = new InSim();
+
+      inSim.on(PacketType.ISP_TINY, (packet) => {
+        expect(packet.SubT).toEqual(TinyType.TINY_NONE);
+      });
+
+      mitm.on('connection', (socket) => {
+        socket.write(
+          new Uint8Array([
+            1, // Size / 4
+            3, // PacketType.ISP_TINY
+            0, // ReqI
+            0, // TinyType.TINY_NONE
+          ]),
+        );
+        socket.on('data', (data) => {
+          expect(data).toEqual(
+            Buffer.from([
+              1, // Size / 4
+              3, // PacketType.ISP_TINY
+              0, // ReqI
+              0, // TinyType.TINY_NONE
+            ]),
+          );
+          inSim.disconnect();
+          done();
+        });
+      });
+
+      inSim.connect({
+        Host: '127.0.0.1',
+        Port: 29999,
+      });
+    });
+  });
+});
