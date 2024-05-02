d1a80abec46510483196309bf219f70289aee347
Martin Kapal <flamecze@gmail.com>
2024-05-02 09:46:11 +0200

    fix: export InSimError class value

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export type { InSimError } from './errors';
+export { InSimError } from './errors';
 export type { InSimOptions } from './InSim';
 export { InSim } from './InSim';
 export type { InSimEvents, InSimPacketEvents } from './InSimEvents';
