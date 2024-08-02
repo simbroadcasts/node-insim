7bf3833964fa13c1045bad3308ffff6042f76ceb
Martin Kapal <flamecze@gmail.com>
2024-08-02 15:06:37 +0200

    docs: wrap long lines in Inst doc text

--- src/packets/IS_BTN.ts
+++ src/packets/IS_BTN.ts
@@ -71,8 +71,12 @@ export class IS_BTN extends SendablePacket {
    * Mainly used internally by InSim but also provides some extra user flags
    *
    * NOTE: You should not use {@link INST_ALWAYS_ON} for most buttons.
-   * This is a special flag for buttons that really must be on in all screens (including the garage and options screens). You will probably need to confine these buttons to the top or bottom edge of the screen, to avoid overwriting LFS buttons. Most buttons should be defined without this flag, and positioned in the recommended area so LFS can keep a space clear in the main screens.
-   *
+   * This is a special flag for buttons that really must be on in all screens
+   * (including the garage and options screens). You will probably need to
+   * confine these buttons to the top or bottom edge of the screen, to avoid
+   * overwriting LFS buttons. Most buttons should be defined without this flag,
+   * and positioned in the recommended area so LFS can keep a space clear in
+   * the main screens.
    * */
   @byte() Inst = 0;
 
