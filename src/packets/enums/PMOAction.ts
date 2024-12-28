aa141145b671f2210999116a6aa4855cac43e0f0
Martin Kapal <flamecze@gmail.com>
2024-12-28 09:50:22 +0100

    docs: add more detailed description of `PMO_GET_Z`

--- src/packets/enums/PMOAction.ts
+++ src/packets/enums/PMOAction.ts
@@ -20,9 +20,24 @@ export enum PMOAction {
   /** Set a connection's layout editor selection */
   PMO_SELECTION,
 
-  /** User pressed O without anything selected */
+  /**
+   * User pressed O without anything selected
+   * 
+   * An {@link IS_AXM} with PMO_POSITION is sent with a single object in the packet if a user
+   * presses O without any object type selected. Information only - no object is added.
+   * The only valid values in Info are X, Y, Zbyte and Heading.
+   */
   PMO_POSITION,
 
-  /** Request Z values / reply with Z values */
+  /**
+   * Request Z values / reply with Z values
+   *
+   * `PMO_GET_Z` can be used to request the resulting Zbyte values for given X, Y, Zbyte
+   * positions listed in the {@link IS_AXM}. A similar reply (information only) will be sent
+   * with adjusted Zbyte values. Index and Heading are ignored and set to zero in the
+   * reply. Flags is set to 0x80 if Zbyte was successfully adjusted, zero if not.
+   * Suggested input values for Zbyte are either 240 to get the highest point at X, Y
+   * or you may use the approximate altitude (see layout file format).
+   */
   PMO_GET_Z,
 }
