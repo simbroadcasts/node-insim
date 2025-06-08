cf184b622c1ac817189fe9136e2c8114b89bc894
Martin Kapal <flamecze@gmail.com>
2025-06-08 14:56:35 +0200

    chore: fix Vitest root & remove Jest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    root: 'src',
+    include: ['src/**/*.test.ts'],
     coverage: {
       include: ['src/**/*.ts'],
       reporter: ['json-summary'],
