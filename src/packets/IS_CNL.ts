007a616056195056612a51d8621a162e9bfcb065
Martin Kapal <flamecze@gmail.com>
2025-09-06 17:52:34 +0200

    WIP

--- src/packets/IS_CNL.ts
+++ src/packets/IS_CNL.ts
@@ -1,6 +1,29 @@
+import { z } from 'zod';
+
 import { byte } from '../decorators';
 import { Packet } from './base';
 import { PacketType } from './enums';
+
+const utf8ToBytes = z.codec(z.instanceof(Uint8Array), z.string(), {
+  encode: (str) => new TextEncoder().encode(str) as Uint8Array<ArrayBuffer>,
+  decode: (bytes) => new TextDecoder().decode(bytes),
+});
+
+const packet = z.object({
+  Text: utf8ToBytes,
+});
+
+const binaryPacket = z.codec(z.instanceof(Uint8Array), packet, {
+  encode: (packet) => z.encode(packet.Text),
+  decode: (bytes) => {},
+});
+
+const binary = packet.encode({
+  Text: 'aa',
+});
+
+utf8ToBytes.encode('Hello, 世界!'); // => Uint8Array
+// utf8ToBytes.encode(bytes); // => "Hello, 世界!"
 
 /**
  * ConN Leave
