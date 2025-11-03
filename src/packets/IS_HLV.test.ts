c03b55e6ecef48242d0bc55d59014df43b0e3375
Martin Kapal <flamecze@gmail.com>
2025-11-03 22:20:08 +0100

    feat(insim): increase `IS_HLV` packet to allow time in milliseconds
    BREAKING CHANGE: `Time` property in `IS_HLV` is in milliseconds

--- src/packets/IS_HLV.test.ts
+++ src/packets/IS_HLV.test.ts
@@ -4,13 +4,13 @@ import { PacketType } from './enums';
 import { HLVCViolation, IS_HLV } from './IS_HLV';
 import { CarContOBJ } from './structs';
 
-const size = 16;
+const size = 20;
 
 const data: PacketTestData<IS_HLV> = {
   ReqI: 0,
   PLID: 3,
   HLVC: HLVCViolation.WALL,
-  Time: 1994,
+  Time: 151258902,
   C: new CarContOBJ({
     Direction: 2,
     Heading: 231,
@@ -28,8 +28,12 @@ const buffer = new Uint8Array([
   3, // PLID
   1, // HLVC
   0, // Sp1
-  202, // C - Time (1)
-  7, // C - Time (1)
+  0, // SpW (1)
+  0, // SpW (2)
+  22, // C - Time (1)
+  7, // C - Time (2)
+  4, // C - Time (3)
+  9, // C - Time (4)
   2, // C - Direction
   231, // C - Heading
   4, // C - Speed
