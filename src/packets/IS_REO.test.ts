c2b2627977c73ba7b8d53e4454fd1db4040cadbb
Martin Kapal <flamecze@gmail.com>
2025-11-03 19:15:25 +0100

    feat(insim): `REO_MAX_PLAYERS` in `IS_REO` increased to 48

--- src/packets/IS_REO.test.ts
+++ src/packets/IS_REO.test.ts
@@ -4,15 +4,16 @@ import { PacketType } from './enums';
 import type { IS_REO_Data } from './IS_REO';
 import { IS_REO } from './IS_REO';
 
-const size = 44;
+const size = 52;
 
 const plIds = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
+  42, 43, 44, 45, 46, 47, 48,
 ];
 
 const instructionData: IS_REO_Data = {
-  NumP: 40,
+  NumP: 48,
   PLID: plIds,
 };
 
@@ -20,8 +21,8 @@ const instructionBuffer = new Uint8Array([
   size / new IS_REO().SIZE_MULTIPLIER, // Size
   36, // Type
   0, // ReqI
-  40, // NumP
-  1, // PLID[40]
+  48, // NumP
+  1, // PLID[48]
   2,
   3,
   4,
@@ -61,11 +62,19 @@ const instructionBuffer = new Uint8Array([
   38,
   39,
   40,
+  41,
+  42,
+  43,
+  44,
+  45,
+  46,
+  47,
+  48,
 ]);
 
 const infoData: Omit<PacketTestData<IS_REO>, 'ReqI'> & { ReqI: number } = {
   ReqI: 2,
-  NumP: 40,
+  NumP: 48,
   PLID: plIds,
 };
 
@@ -73,8 +82,8 @@ const infoBuffer = new Uint8Array([
   size / new IS_REO().SIZE_MULTIPLIER, // Size
   36, // Type
   2, // ReqI
-  40, // NumP
-  1, // PLID[40]
+  48, // NumP
+  1, // PLID[48]
   2,
   3,
   4,
@@ -114,6 +123,14 @@ const infoBuffer = new Uint8Array([
   38,
   39,
   40,
+  41,
+  42,
+  43,
+  44,
+  45,
+  46,
+  47,
+  48,
 ]);
 
 describe('IS_REO', () => {
@@ -132,14 +149,14 @@ describe('IS_REO', () => {
     buffer: infoBuffer,
   });
 
-  it('should throw a range error if PLID length is greater than 40', () => {
+  it('should throw a range error if PLID length is greater than 48', () => {
     expect(() => {
       new IS_REO({
-        NumP: 41,
+        NumP: 49,
         PLID: [
           1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
           21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
-          38, 39, 40, 41,
+          38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
         ],
       }).pack();
     }).toThrow(RangeError);
