49efc9d5766bb011b904d6c3aacfdaea37b6d4bf
Martin Kapal <flamecze@gmail.com>
2026-08-24 20:52:28 +0200

    feat(IS_BTN): add `IS_BTN.MAX_TEXT_LENGTH` static class property (240)

---
+++ .changeset/bumpy-crews-show.md
@@ -0,0 +1,5 @@
+---
+'node-insim': minor
+---
+
+Add `IS_BTN.MAX_TEXT_LENGTH` static class property (240)
--- src/packets/IS_BTN.ts
+++ src/packets/IS_BTN.ts
@@ -37,6 +37,7 @@ export class IS_BTN extends SendablePacket {
   public static readonly IS_Y_MIN = 30;
   public static readonly IS_Y_MAX = 170;
   public static readonly MAX_CLICK_ID = 239;
+  public static readonly MAX_TEXT_LENGTH = 240;
   public static readonly FIXED_DATA_SIZE = 12;
 
   /** 12 + text size (a multiple of 4) */
